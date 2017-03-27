804e418ee6dc869c15c2d614fe935d70c955d203
Pepe Valdivia <jvaldivia@softweb.pe>
2017-03-27 17:37:52 -0500

    Commit inicial

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.*;
+
+//import views.html.*;
+import views.html.home.*;
+
+public class HomeController extends Controller {
+
+    public Result index() {
+        //return ok(index.render("Your new application is ready???."));
+    	return ok(index.render("hola desde home controller"));
+    }
+
+}
